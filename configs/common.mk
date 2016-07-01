6227dc2faffda5a17303ce883b3e31b0ec432340
renosang <renosang@gmail.com>
2016-07-02 00:26:31 +0700

    Chooser default vietnamese lang

--- configs/common.mk
+++ configs/common.mk
@@ -104,6 +104,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1
 
+# Default Vietnamese Lang
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.product.locale.language=vi
+	persist.product.locale.region=VN
+
 # Thank you, please drive thru!
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.dun.override=0
