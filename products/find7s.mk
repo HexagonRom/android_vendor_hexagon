d1b3ddc1576e1a8699807ca7d3a1723c2245e591
renosang <renosang@gmail.com>
2016-07-14 08:52:35 +0700

    Fix build Find 7s

--- products/find7s.mk
+++ products/find7s.mk
@@ -1,4 +1,10 @@
+# Inherit framework first
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 $(call inherit-product, vendor/hexagon/configs/common.mk)
+
+# Inherit telephony stuff
+$(call inherit-product, vendor/hexagon/configs/telephony.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/hexagon/configs/nfc_enhanced.mk)
@@ -7,15 +13,19 @@ $(call inherit-product, vendor/hexagon/configs/nfc_enhanced.mk)
 $(call inherit-product, device/oppo/find7s/full_find7s.mk)
 
 
+
 PRODUCT_NAME := hexagon_find7s
 PRODUCT_DEVICE := find7s
-PRODUCT_BRAND := Oppo
+PRODUCT_BRAND := OPPO
+PRODUCT_MANUFACTURER := OPPO
 PRODUCT_MODEL := Find7
-PRODUCT_MANUFACTURER := Oppo
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=4.4.2/KVT49L/1390465867:user/release-keys \
-    PRIVATE_BUILD_DESC="msm8974-user 4.4.2 KVT49L eng.root.20141017.144947 release-keys"
+    BUILD_FINGERPRINT=5.1.1/LMY47V/1441693976:user/release-keys \
+    PRIVATE_BUILD_DESC="msm8974-user 5.1.1 LMY47V 153 release-keys" \
+TARGET_DEVICE="FIND7"
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
