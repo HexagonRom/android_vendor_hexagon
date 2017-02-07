0088abe00dfba2d83a7743a63aa8a1a25c651aea
razorloves <razorloves@gmail.com>
2017-02-07 22:55:19 +0300

    angler: update build fingerprint & description to N4F26O

--- products/angler.mk
+++ products/angler.mk
@@ -21,8 +21,8 @@ TARGET_VENDOR := huawei
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MTC20F/3031278:user/release-keys \
-    PRIVATE_BUILD_DESC="angler-user 6.0.1 MTC20F 3031278 release-keys"
+    BUILD_FINGERPRINT=google/angler/angler:7.1.1/N4F26O/3582057:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 7.1.1 N4F26O 3582057 release-keys"
 
 # hexagon Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
