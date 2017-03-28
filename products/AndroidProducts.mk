faad622647398fa4ef5991bc07d2a4f649bf81ed
EsromG5 <esromgalaxy5@gmail.com>
2017-03-28 21:57:13 +0000

    Add device a6000

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,4 +1,5 @@
 PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/a6000.mk \
     $(LOCAL_DIR)/amami.mk \
     $(LOCAL_DIR)/angler.mk \
     $(LOCAL_DIR)/armani.mk \
---
+++ products/a6000.mk
@@ -0,0 +1,40 @@
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$(call inherit-product, device/lenovo/a6000/full_a6000.mk)
+
+# Inherit some common hexagon stuff.
+$(call inherit-product, vendor/hexagon/configs/common.mk)
+
+# Inherit telephony stuff
+$(call inherit-product, vendor/hexagon/configs/telephony.mk)
+
+# Must define platform variant before including any common things
+TARGET_BOARD_PLATFORM_VARIANT := msm8916
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := Kraft-T,a6000,K30-T,A6000,Kraft-W,Kraft-C,k30t,msm8916,Kraft-A6000,wt86518
+
+PRODUCT_NAME := hexagon_a6000
+BOARD_VENDOR := Lenovo
+
+PRODUCT_GMS_CLIENTID_BASE := android-lenovo
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	DEVICE_MAINTAINERS="MR.HACKER5476 & Has.007"
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+-include vendor/hexagon/configs/bootanimation.mk
