c85cf04cf95a79c7aca154eeb67c3187f2f1712c
Rygebin <kaankulahli2@gmail.com>
2017-04-25 17:01:44 +0000

    shamrock: Fix typo's

--- products/shamrock.mk
+++ products/shamrock.mk
@@ -23,7 +23,7 @@ $(call inherit-product, vendor/hexagon/configs/common.mk)
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8952
 
-PRODUCT_NAME := aicp_shamrock
+PRODUCT_NAME := hexagon_shamrock
 BOARD_VENDOR := google
 PRODUCT_DEVICE := shamrock
 
