df56cd77d64d61a7bff4db0c673324989389d368
Lütfi Akdağ <harddeath13@gmail.com>
2017-06-23 22:00:57 +0300

    Add crackling to Official devices

---
+++ products/crackling.mk
@@ -0,0 +1,46 @@
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$(call inherit-product, device/wileyfox/crackling/full_crackling.mk)
+
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/hexagon/configs/common.mk)
+
+# Inherit telephony stuff
+$(call inherit-product, vendor/hexagon/configs/telephony.mk)
+
+# Must define platform variant before including any common things
+TARGET_BOARD_PLATFORM_VARIANT := msm8916
+
+PRODUCT_NAME := hexagon_crackling
+BOARD_VENDOR := wileyfox
+PRODUCT_DEVICE := crackling
+
+PRODUCT_GMS_CLIENTID_BASE := android-wileyfox
+
+PRODUCT_MANUFACTURER := Wileyfox
+PRODUCT_MODEL := Wileyfox Swift
+
+PRODUCT_BRAND := Wileyfox
+TARGET_VENDOR := wileyfox
+TARGET_VENDOR_PRODUCT_NAME := Swift
+TARGET_VENDOR_DEVICE_NAME := crackling
+
+# HexagonRom Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="Mehmet Dalverir (Hybriss)"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=Wileyfox/Swift/crackling:7.1.1/N6F26Y/5506d7d322:user/release-keys \
+    PRIVATE_BUILD_DESC="crackling-user 7.1.1 N6F26Y 5506d7d322 release-keys"
