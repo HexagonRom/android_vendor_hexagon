07bd67da7c135bdbac6c23e3f19bcfe795a84b6c
Sushant More <moresushant48@gmail.com>
2017-07-29 13:23:50 +0000

    Set default BUILDTYPE to UNOFFICIAL
    Signed-off-by: Sushant More <moresushant48@gmail.com>

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -16,7 +16,7 @@ ifndef HEXAGON_BUILDTYPE
         RELEASE_TYPE := $(shell echo $(RELEASE_TYPE) | sed -e 's|^HEXAGON_||g')
         HEXAGON_BUILDTYPE := $(RELEASE_TYPE)
     else
-        HEXAGON_BUILDTYPE := OFFICIAL
+        HEXAGON_BUILDTYPE := UNOFFICIAL
     endif
 endif
 
