3af6104a1fb7d3b28fc4f3267fbf28ac1ae8ca8f
Remilia Scarlet <remilia.1505@gmail.com>
2017-08-02 04:39:30 +0000

    che10: add support devices

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -12,6 +12,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/capricorn.mk \
     $(LOCAL_DIR)/chagalllte.mk \
     $(LOCAL_DIR)/chagallwifi.mk \
+    $(LOCAL_DIR)/che10.mk \
     $(LOCAL_DIR)/clark.mk \
     $(LOCAL_DIR)/condor.mk \
     $(LOCAL_DIR)/crackling.mk \
---
+++ products/che10.mk
@@ -0,0 +1,34 @@
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from these products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/huawei/che10/device.mk)
+
+# Inherit some common stuff.
+$(call inherit-product, vendor/hexagon/configs/common.mk)
+
+# Inherit telephony stuff
+$(call inherit-product, vendor/hexagon/configs/telephony.mk)
+
+PRODUCT_DEVICE := che10
+PRODUCT_NAME := hexagon_che10
+PRODUCT_BRAND := Huawei
+PRODUCT_MODEL := che10
+PRODUCT_MANUFACTURER := HUAWEI
+
+PRODUCT_GMS_CLIENTID_BASE := android-huawei
