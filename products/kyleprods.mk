0cb7aed6b5acdde7693ed13c9bd0a76344581f62
Real man born on semtember <hieunam03@gmail.com>
2017-08-19 13:37:48 +0700

    Update kyleprods.mk

--- products/kyleprods.mk
+++ products/kyleprods.mk
@@ -8,7 +8,7 @@ $(call inherit-product, vendor/hexagon/configs/common.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/kylepro/device_kyleprods.mk)
+$(call inherit-product, device/samsung/kyleprods/device_kyleprods.mk)
 
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=kyleproxx TARGET_DEVICE=kyleprods BUILD_FINGERPRINT="samsung/kyleproxx/kyleprods:4.2.2/SME13A/S7582XXUAMK5:user/release-keys" PRIVATE_BUILD_DESC="kyleproxx-user 4.2.2 SME13A S7582XXUAMK5 release-keys"
