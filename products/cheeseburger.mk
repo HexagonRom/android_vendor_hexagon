a6077c20f97b02d7cb36496a219654ba4f1e54ac
renosang <renosang@gmail.com>
2017-12-06 19:03:32 +0000

    cheeseburger: Update base firmware to OxygenOS 4.5.14

---
+++ products/cheeseburger.mk
@@ -0,0 +1,51 @@
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from oneplus3 device
+$(call inherit-product, device/oneplus/cheeseburger/device.mk)
+
+# Inherit some common stuff.
+$(call inherit-product, vendor/hexagon/configs/common.mk)
+
+# Inherit telephony stuff
+$(call inherit-product, vendor/hexagon/configs/telephony.mk)
+
+PRODUCT_BRAND := OnePlus
+PRODUCT_DEVICE := cheeseburger
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_MODEL := ONEPLUS A5000
+PRODUCT_NAME := hexagon_cheeseburger
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+TARGET_VENDOR := oneplus
+
+TARGET_VENDOR_PRODUCT_NAME := OnePlus5
+TARGET_VENDOR_DEVICE_NAME := OnePlus5
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=OnePlus5 PRODUCT_NAME=OnePlus5
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=OnePlus/OnePlus5/OnePlus5:7.1.1/NMF26X/10171617:user/release-keys
+
+PRODUCT_PROPERTY_OVERRIDES += \
+        ro.build.oemfingerprint=7.1.1/NMF26X/10171617:user/release-keys
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+-include vendor/hexagon/configs/bootanimation.mk
