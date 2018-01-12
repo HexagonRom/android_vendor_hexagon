DirtyUnicorns/android_vendor_du

Fix Google dialer
Add in dialer_experience.xml found in the factory image so if the user
decides to install it, they're not meant with a message about how is not
supported.

Change-Id: I372ff394e3704dd6380812140a4e35cfbfb8f852

config/common.mk
@@ -39,6 +39,10 @@ PRODUCT_COPY_FILES += \
     vendor/du/prebuilt/common/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
 endif
 
+# Fix Google dialer
+PRODUCT_COPY_FILES += \
+    vendor/du/prebuilt/common/etc/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml
+
 # Don't export PS1 in /system/etc/mkshrc.
 PRODUCT_COPY_FILES += \
     vendor/du/prebuilt/common/etc/mkshrc:system/etc/mkshrc

HexagonRom/android_vendor_hexagon

config/common.mk
@@ -96,6 +96,10 @@ PRODUCT_COPY_FILES += \
 # Include Hexagon audio files
 include vendor/hexagon/config/hexagon_audio.mk
 
+# Fix Google dialer
+PRODUCT_COPY_FILES += \
+    vendor/aicp/prebuilt/common/etc/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml
+
 ifneq ($(TARGET_DISABLE_LINEAGE_SDK), true)
 # Lineage SDK
 include vendor/hexagon/config/lineage_sdk_common.mk